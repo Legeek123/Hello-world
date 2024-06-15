e4b62829e94cf433f9a3594b379dba503a0b8773
Legeek123 <162630260+Legeek123@users.noreply.github.com>
2024-06-15 17:34:47 +0200

    parallax effect

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import SimpleParallax from "simple-parallax-js/vanilla";
 
-var image = document.getElementsByClassName('thumbnail');
+var image = document.getElementsByClassName('img');
 new simpleParallax(image);
 
 //You can also choose to apply the parallax on multiple images:
 
-var images = document.querySelectorAll('img');
+var images = document.querySelectorAll('img1.png');
 new simpleParallax(images);
