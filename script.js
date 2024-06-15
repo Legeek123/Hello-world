cd9c6d008d44f06401c61c1422810423e0f9b715
Legeek123 <162630260+Legeek123@users.noreply.github.com>
2024-06-15 17:45:02 +0200

    parallax

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import SimpleParallax from "simple-parallax-js/vanilla";
 
-var image = document.getElementsByClassName('img');
+var image = document.getElementsByClassName('parallax');
 new simpleParallax(image);
 
 //You can also choose to apply the parallax on multiple images:
 
-var images = document.querySelectorAll('img1.png');
+var images = document.getElementById('para');
 new simpleParallax(images);
